d9112c853156c2b6f8158f42868ccca9614d4005
ianspractice <bodybody.ian@gmail.com>
2025-05-21 14:00:19 -0600

    Transform each element of the array into a string

--- substrings.rb
+++ substrings.rb
@@ -2,6 +2,12 @@ dictionary = ["below","below","down","go","going","horn","how","howdy","it","i",
 
 
 # turn each index of array into a string
+def substrings(str, array)
+  array.each { |index| puts index.to_s }
+end
+
+
+puts substrings("str", dictionary)
 # check if string contains the word
 # if true add 1 to hash
 # return has
