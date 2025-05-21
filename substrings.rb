9d3a5136e49cf74a0e907189fea7170636e57e4f
ianspractice <bodybody.ian@gmail.com>
2025-05-21 13:57:47 -0600

    Create directions for project

---
+++ substrings.rb
@@ -0,0 +1,7 @@
+dictionary = ["below","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+
+# turn each index of array into a string
+# check if string contains the word
+# if true add 1 to hash
+# return has
