88e6e3c9cda22a1138632a26799966458515c11b
ianspractice <bodybody.ian@gmail.com>
2025-05-21 14:05:25 -0600

    Definition works with single words in array

--- substrings.rb
+++ substrings.rb
@@ -3,11 +3,18 @@ dictionary = ["below","below","down","go","going","horn","how","howdy","it","i",
 
 # turn each index of array into a string
 def substrings(str, array)
-  array.each { |index| puts index.to_s }
+  hash = Hash.new{0}
+  array.each do |index| 
+    substr = index.to_s
+    if str.include?(substr)
+      hash[substr] += 1
+    end
+  end
+hash
 end
 
 
-puts substrings("str", dictionary)
+puts substrings("below", dictionary)
 # check if string contains the word
 # if true add 1 to hash
 # return has
