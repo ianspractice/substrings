9b44a0460424c54450a577df332ac5784564fae3
ianspractice <bodybody.ian@gmail.com>
2025-05-21 14:24:08 -0600

    Works for phrases

--- substrings.rb
+++ substrings.rb
@@ -4,10 +4,14 @@ dictionary = ["below","below","down","go","going","horn","how","howdy","it","i",
 # turn each index of array into a string
 def substrings(str, array)
   hash = Hash.new{0}
+  #transform each array index into a string
   array.each do |index| 
     substr = index.to_s
-    str.split(" ").each do |index2|
+    #transform str argument into lowercase individual strings
+    str.downcase.split(" ").each do |index2|
+    # check if string contains the word
     if index2.include?(substr)
+    # if true add 1 to hash
       hash[substr] += 1
     end
     end
@@ -17,6 +21,3 @@ end
 
 
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
-# check if string contains the word
-# if true add 1 to hash
-# return has
